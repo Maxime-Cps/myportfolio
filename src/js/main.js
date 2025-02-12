719e2cf24cf4c726ee766b889e7570443d71e69c
maxime.chapuis <maxime.chapuis@etu.unistra.fr>
2025-02-12 19:09:49 +0100

    Modified Projects + New LinkedIn button

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,11 @@ document.getElementById('cv-button').addEventListener('click', function() {
 document.getElementById('github').addEventListener('click', function() {
     window.open('https://github.com/Maxime-Cps', '_blank');
 });
+
+document.getElementById('linkedIn').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/maxime-chapuis-dev/', '_blank');
+});
+
+document.getElementById('linkedIn2').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/maxime-chapuis-dev/', '_blank');
+});
