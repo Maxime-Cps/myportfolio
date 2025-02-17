5e3a61aca8ac34e387ad8a0a1dbf6301073841fa
Maxime Chapuis <maxime.chapuis60@gmail.com>
2025-02-17 14:57:09 +0100

    New contact page

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,7 @@ document.getElementById('linkedIn').addEventListener('click', function() {
 document.getElementById('linkedIn2').addEventListener('click', function() {
     window.open('https://www.linkedin.com/in/maxime-chapuis-dev/', '_blank');
 });
+
+document.getElementById('sendEmail').addEventListener('click', function() {
+    window.open('mailto:maxime.chapuis60@gmail.com', '_blank');
+});
